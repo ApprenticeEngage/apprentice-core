b5a1328e4044c42ae53beebc54c3e290bf7e4064
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-23 17:16:19 +0500

    app architecture configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
+import { ApprenticeModule } from './apprentice/apprentice.module';
+import { MentorModule } from './mentor/mentor.module';
 
 @Module({
-  imports: [],
+  imports: [CoursesModule, ApprenticeModule, MentorModule],
   controllers: [AppController],
   providers: [AppService],
 })
