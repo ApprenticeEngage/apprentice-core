7048e9be658a120ea53ccea797929ba743f38ed3
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-23 18:05:32 +0500

    added prisma migrations of apprentice to the repo

--- src/courses/controller/controller.controller.ts
+++ src/courses/controller/controller.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UUID } from 'crypto';
 
 @Controller('controller')
-export class ControllerController {}
+export class ControllerController {
+  //for main course page
+  @Get('/main/:courseId')
+  getCourseContent(@Param('courseId') courseId: UUID) {
+    
+  }
+}
