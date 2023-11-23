5822ac1b3f4725b07f72d3b6e500809dfbea5906
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-23 18:30:03 +0500

    added db service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { ApprenticeModule } from './apprentice/apprentice.module';
 import { MentorModule } from './mentor/mentor.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CoursesModule, ApprenticeModule, MentorModule],
+  imports: [CoursesModule, ApprenticeModule, MentorModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/courses/controller/controller.controller.ts
+++ src/courses/controller/controller.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { UUID } from 'crypto';
 
 @Controller('controller')
 export class ControllerController {
   //for main course page
   @Get('/main/:courseId')
-  getCourseContent(@Param('courseId') courseId: UUID) {
-    
-  }
+  getCourseContent(@Param('courseId') courseId: string) {}
 }
