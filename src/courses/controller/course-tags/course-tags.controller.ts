eece01764d7a7510f1e78dcd43d1cd32ede8e166
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-25 18:58:26 +0500

    crud apis for course tags done

--- src/courses/controller/course-tags/course-tags.controller.ts
+++ src/courses/controller/course-tags/course-tags.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CourseTag } from '@prisma/client';
+import { CoursesTagsService } from 'src/courses/service/courses-tags/courses-tags.service';
 
 @Controller('course-tags')
-export class CourseTagsController {}
+export class CourseTagsController {
+  constructor(private readonly service: CoursesTagsService) {}
+  //find lo of a courseID
+  @Get('/:courseId')
+  async getCourseContent(@Param('courseId', ParseIntPipe) courseId: number) {
+    return this.service.findAll(courseId);
+  }
+
+  //store course tag of a courseID
+  @Post()
+  create(@Body() courseObj: CourseTag) {
+    return this.service.create(courseObj);
+  }
+
+  //update a specific learning outcome
+  @Put('/:courseId/:serialNo')
+  update(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+    @Body() courseTag: CourseTag,
+  ) {
+    return this.service.update(courseId, serialNo, courseTag);
+  }
+
+  //remove a specific Course Tag
+  @Delete('/:courseId/:serialNo')
+  delete(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+  ) {
+    return this.service.remove(courseId, serialNo);
+  }
+}
--- src/courses/service/courses-tags/courses-tags.service.ts
+++ src/courses/service/courses-tags/courses-tags.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { CourseTag } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class CoursesTagsService {}
+export class CoursesTagsService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  //both mentor and apprentice can invoke this
+  async findAll(courseId: number) {
+    return this.dataBase.courseTag.findMany({
+      where: {
+        course_id: courseId,
+      },
+    });
+  }
+
+  //this privilege only resides with the mentor ; so setup auth as such
+  async create(courseTag: CourseTag) {
+    return this.dataBase.courseTag.create({ data: courseTag });
+  }
+
+  async remove(courseId: number, serialNo: number) {
+    try {
+      return await this.dataBase.courseTag.delete({
+        where: {
+          course_id_serialNo: {
+            course_id: courseId,
+            serialNo: serialNo,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to remove courseTag: ${error.message}`);
+    }
+  }
+
+  async update(courseId: number, serialNo: number, courseTag: CourseTag) {
+    return this.dataBase.courseTag.update({
+      where: {
+        course_id_serialNo: {
+          course_id: courseId,
+          serialNo: serialNo,
+        },
+      },
+      data: courseTag,
+    });
+  }
+}
