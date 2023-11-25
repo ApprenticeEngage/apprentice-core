bc38535eb9515cc4472a2375c1001467666ae4ab
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-25 18:45:58 +0500

    prisma changes to DB

--- src/courses/controller/course-pre-req/course-pre-req.controller.ts
+++ src/courses/controller/course-pre-req/course-pre-req.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CoursePreReq } from '@prisma/client';
+import { CoursesPreReqService } from 'src/courses/service/courses-pre-req/courses-pre-req.service';
 @Controller('course-pre-req')
-export class CoursePreReqController {}
+export class CoursePreReqController {
+  constructor(private readonly service: CoursesPreReqService) {}
+  //find lo of a courseID
+  @Get('/:courseId')
+  async getCourseContent(@Param('courseId', ParseIntPipe) courseId: number) {
+    return this.service.findAll(courseId);
+  }
+
+  //store prereq of a courseID
+  @Post()
+  create(@Body() courseObj: CoursePreReq) {
+    return this.service.create(courseObj);
+  }
+
+  //update a specific learning outcome
+  @Put('/:courseId/:serialNo')
+  update(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+    @Body() courseLo: CoursePreReq,
+  ) {
+    return this.service.update(courseId, serialNo, courseLo);
+  }
+
+  //remove a specific prereq
+  @Delete('/:courseId/:serialNo')
+  delete(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+  ) {
+    return this.service.remove(courseId, serialNo);
+  }
+}
--- src/courses/service/courses-pre-req/courses-pre-req.service.ts
+++ src/courses/service/courses-pre-req/courses-pre-req.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { CoursePreReq } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class CoursesPreReqService {}
+export class CoursesPreReqService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  //both mentor and apprentice can invoke this
+  async findAll(courseId: number) {
+    return this.dataBase.coursePreReq.findMany({
+      where: {
+        course_id: courseId,
+      },
+    });
+  }
+
+  //this privilege only resides with the mentor ; so setup auth as such
+  async create(coursePreReq: CoursePreReq) {
+    return this.dataBase.coursePreReq.create({ data: coursePreReq });
+  }
+
+  async remove(courseId: number, serialNo: number) {
+    try {
+      return await this.dataBase.coursePreReq.delete({
+        where: {
+          course_id_serialNo: {
+            course_id: courseId,
+            serialNo: serialNo,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to remove coursePreReq: ${error.message}`);
+    }
+  }
+
+  async update(courseId: number, serialNo: number, coursePreReq: CoursePreReq) {
+    return this.dataBase.coursePreReq.update({
+      where: {
+        course_id_serialNo: {
+          course_id: courseId,
+          serialNo: serialNo,
+        },
+      },
+      data: coursePreReq,
+    });
+  }
+}
