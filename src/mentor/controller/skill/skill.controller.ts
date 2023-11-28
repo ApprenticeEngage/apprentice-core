5dd690aae228b93220ebdeee04d4316875f2b1f7
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 20:15:53 +0500

    controllers and services for skilll table

--- src/mentor/controller/skill/skill.controller.ts
+++ src/mentor/controller/skill/skill.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Skill } from '@prisma/client';
+import { SkillService } from 'src/mentor/service/skill/skill.service';
 
 @Controller('skill')
-export class SkillController {}
+export class SkillController {
+  constructor(private readonly service: SkillService) {}
+
+  @Post('/create')
+  create(@Body() skill: Skill) {
+    return this.service.create(skill);
+  }
+
+  @Get('/:id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOne(id);
+  }
+
+  //updating a skill
+  @Put('/:id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() skill: Skill) {
+    return this.service.update(id, skill);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.service.remove(id);
+  }
+}
--- src/mentor/service/skill/skill.service.ts
+++ src/mentor/service/skill/skill.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { Skill } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class SkillService {}
+export class SkillService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  create(skill: Skill) {
+    return this.dataBase.skill.create({ data: skill });
+  }
+
+  findOne(id: number) {
+    return this.dataBase.skill.findMany({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  update(id: number, skill: Skill) {
+    return this.dataBase.skill.update({
+      where: {
+        id: id,
+      },
+      data: skill,
+    });
+  }
+
+  remove(id: number) {
+    return this.dataBase.skill.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
