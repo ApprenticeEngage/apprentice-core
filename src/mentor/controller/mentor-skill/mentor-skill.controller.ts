0f11bf7ee809ecf9fead09d3eb7a83cfea31152d
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 20:27:25 +0500

    controllers and services for Mentor-skilll table

--- src/mentor/controller/mentor-skill/mentor-skill.controller.ts
+++ src/mentor/controller/mentor-skill/mentor-skill.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { MentorSkill } from '@prisma/client';
+import { MentorSkillService } from 'src/mentor/service/mentor-skill/mentor-skill.service';
 
-@Controller('mentor-skill')
-export class MentorSkillController {}
+@Controller('mentor/mentor-skill')
+export class MentorSkillController {
+  constructor(private readonly service: MentorSkillService) {}
+
+  @Post('/create')
+  create(@Body() skill: MentorSkill) {
+    return this.service.create(skill);
+  }
+
+  @Get('/:id/:skillId')
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('skillId', ParseIntPipe) skillId: number,
+  ) {
+    return this.service.findOne(id, skillId);
+  }
+
+  //updating a skill
+  @Put('/:id/:skillId')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('id', ParseIntPipe) skillId: number,
+    @Body() skill: MentorSkill,
+  ) {
+    return this.service.update(id, skillId, skill);
+  }
+
+  @Delete('/:id/:skillId')
+  remove(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('id', ParseIntPipe) skillId: number,
+  ) {
+    return this.service.remove(id, skillId);
+  }
+}
--- src/mentor/service/mentor-skill/mentor-skill.service.ts
+++ src/mentor/service/mentor-skill/mentor-skill.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { MentorSkill } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class MentorSkillService {}
+export class MentorSkillService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  create(skill: MentorSkill) {
+    return this.dataBase.mentorSkill.create({ data: skill });
+  }
+
+  async findOne(id: number, skillId: number) {
+    return this.dataBase.mentorSkill.findFirst({
+      where: {
+        mentor_id: id,
+        skill_id: skillId,
+      },
+    });
+  }
+
+  update(id: number, skillId: number, skill: MentorSkill) {
+    return this.dataBase.mentorSkill.update({
+      where: {
+        mentor_id_skill_id: {
+          mentor_id: id,
+          skill_id: skillId,
+        },
+      },
+      data: skill,
+    });
+  }
+
+  remove(id: number, skillId: number) {
+    return this.dataBase.mentorSkill.delete({
+      where: {
+        mentor_id_skill_id: {
+          mentor_id: id,
+          skill_id: skillId,
+        },
+      },
+    });
+  }
+}
