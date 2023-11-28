21f04f996ae279b605c2265e77ce37190d67f7f4
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-29 00:02:12 +0500

    services and controller added for courseTestimonials

--- src/courses/controller/courses-testimonials/courses-testimonials.controller.ts
+++ src/courses/controller/courses-testimonials/courses-testimonials.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CourseTestimonials } from '@prisma/client';
+import { CoursesTestimonialsService } from 'src/courses/service/courses-testimonials/courses-testimonials.service';
 
-@Controller('courses-testimonials')
-export class CoursesTestimonialsController {}
+@Controller('courses/courses-testimonials')
+export class CoursesTestimonialsController {
+  constructor(private readonly service: CoursesTestimonialsService) {}
+  //find all test of course
+  @Get('/course/:courseId')
+  async getCourseTestimonials(
+    @Param('courseId', ParseIntPipe) courseId: number,
+  ) {
+    return this.service.findAllCourseTestimonials(courseId);
+  }
+
+  //find all test of apprentice
+  @Get('/apprentice/:apprenticeId')
+  async getAllApprenticeTestimonials(
+    @Param('apprenticeId', ParseIntPipe) apprenticeId: number,
+  ) {
+    return this.service.findAllApprenticeTestimonials(apprenticeId);
+  }
+  //store course tag of a courseID
+  @Post()
+  create(@Body() courseTestimonials: CourseTestimonials) {
+    return this.service.create(courseTestimonials);
+  }
+
+  //update a specific learning outcome
+  @Put('/:courseId/:serialNo')
+  update(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+    @Body() courseTestimonials: CourseTestimonials,
+  ) {
+    return this.service.update(courseId, serialNo, courseTestimonials);
+  }
+
+  //remove a specific Course Tag
+  @Delete('/:courseId/:serialNo')
+  delete(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('serialNo', ParseIntPipe) serialNo: number,
+  ) {
+    return this.service.remove(courseId, serialNo);
+  }
+}
--- src/courses/service/courses-testimonials/courses-testimonials.service.ts
+++ src/courses/service/courses-testimonials/courses-testimonials.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { CourseTestimonials } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class CoursesTestimonialsService {}
+export class CoursesTestimonialsService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  //both mentor and apprentice can invoke this
+  async findAllCourseTestimonials(courseId: number) {
+    return this.dataBase.courseTestimonials.findMany({
+      where: {
+        course_id: courseId,
+      },
+    });
+  }
+
+  async findAllApprenticeTestimonials(apprenticeId: number) {
+    return this.dataBase.courseTestimonials.findMany({
+      where: {
+        apprentice_id: apprenticeId,
+      },
+    });
+  }
+
+  //this privilege only resides with the mentor ; so setup auth as such
+  async create(courseTestimonials: CourseTestimonials) {
+    return this.dataBase.courseTestimonials.create({
+      data: courseTestimonials,
+    });
+  }
+
+  async remove(courseId: number, serialNo: number) {
+    try {
+      return await this.dataBase.courseTestimonials.delete({
+        where: {
+          course_id_serialNo: {
+            course_id: courseId,
+            serialNo: serialNo,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to remove courseTag: ${error.message}`);
+    }
+  }
+
+  async update(
+    courseId: number,
+    serialNo: number,
+    courseTestimonials: CourseTestimonials,
+  ) {
+    return this.dataBase.courseTestimonials.update({
+      where: {
+        course_id_serialNo: {
+          course_id: courseId,
+          serialNo: serialNo,
+        },
+      },
+      data: courseTestimonials,
+    });
+  }
+}
