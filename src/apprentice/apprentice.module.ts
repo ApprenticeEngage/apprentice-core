12d3a2a243b4010baba9a1dcaf758f2c1335bb74
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 22:46:21 +0500

    reconfig folder structure for apprentice module

--- src/apprentice/apprentice.module.ts
+++ src/apprentice/apprentice.module.ts
@@ -3,8 +3,8 @@ import { ApprenticeService } from './service/service.service';
 import { ApprenticeController } from './controller/controller.controller';
 import { ApprenticeCertController } from './controller/apprentice-cert/apprentice-cert.controller';
 import { ApprenticeCoursesController } from './controller/apprentice-courses/apprentice-courses.controller';
-import { ApprenticeCoursesService } from './services/apprentice-courses/apprentice-courses.service';
-import { ApprenticeCertService } from './services/apprentice-cert/apprentice-cert.service';
+import { ApprenticeCoursesService } from './service/apprentice-courses/apprentice-courses.service';
+import { ApprenticeCertService } from './service/apprentice-cert/apprentice-cert.service';
 
 @Module({
   providers: [ApprenticeService, ApprenticeCoursesService, ApprenticeCertService],
