a06063b52d7a27201a78b3686c89feac3ebcae07
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 23:27:10 +0500

    services adn controllers for apprenticeCert

--- src/apprentice/controller/apprentice-cert/apprentice-cert.controller.ts
+++ src/apprentice/controller/apprentice-cert/apprentice-cert.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApprenticeCert } from '@prisma/client';
+import { ApprenticeCertService } from 'src/apprentice/service/apprentice-cert/apprentice-cert.service';
 @Controller('apprentice/apprentice-cert')
-export class ApprenticeCertController {}
+export class ApprenticeCertController {
+  constructor(private readonly service: ApprenticeCertService) {}
+
+  @Post('/create')
+  create(@Body() apprenticeCert: ApprenticeCert) {
+    return this.service.create(apprenticeCert);
+  }
+
+  @Get('/:id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOne(id);
+  }
+
+  //the provided ID is the id of the entry ; which is a surrogate key
+  @Put('/:id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() apprenticeCert: ApprenticeCert,
+  ) {
+    return this.service.update(id, apprenticeCert);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.service.remove(id);
+  }
+}
--- src/apprentice/service/apprentice-cert/apprentice-cert.service.ts
+++ src/apprentice/service/apprentice-cert/apprentice-cert.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { ApprenticeCert } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class ApprenticeCertService {}
+export class ApprenticeCertService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  create(apprenticeCert: ApprenticeCert) {
+    return this.dataBase.apprenticeCert.create({ data: apprenticeCert });
+  }
+
+  findOne(id: number) {
+    return this.dataBase.apprenticeCert.findFirst({
+      where: {
+        apprentice_id: id,
+      },
+    });
+  }
+
+  update(id: number, apprenticeCert: ApprenticeCert) {
+    return this.dataBase.apprenticeCert.update({
+      where: {
+        //updating via the actual ID
+        id: id,
+      },
+      data: apprenticeCert,
+    });
+  }
+
+  remove(id: number) {
+    return this.dataBase.apprenticeStats.delete({
+      where: {
+        apprentice_id: id,
+      },
+    });
+  }
+}
