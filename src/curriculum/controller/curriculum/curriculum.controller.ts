e95187c507b62089ad4eef799574f4d3e5b52380
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 16:47:18 +0500

    made changes to schema and added controllers for curriculum

--- src/curriculum/controller/curriculum/curriculum.controller.ts
+++ src/curriculum/controller/curriculum/curriculum.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Curriculum } from '@prisma/client';
+import { CurriculumService } from 'src/curriculum/services/curriculum/curriculum.service';
 
 @Controller('curriculum')
-export class CurriculumController {}
+export class CurriculumController {
+  constructor(private readonly service: CurriculumService) {}
+
+  //find lo of a curriculum
+  @Get('/:currId')
+  async getCourseContent(@Param('currId', ParseIntPipe) courseId: number) {
+    return this.service.findAll(courseId);
+  }
+
+  //store LO of a curriculum
+  @Post()
+  create(@Body() curr: Curriculum) {
+    return this.service.create(curr);
+  }
+
+  //update a specific learning outcome
+  @Put('/:currId')
+  update(
+    @Param('currId', ParseIntPipe) courseId: number,
+    @Body() Curriculum: Curriculum,
+  ) {
+    return this.service.update(courseId, Curriculum);
+  }
+
+  //remove a specific LO
+  @Delete('/:currId')
+  delete(@Param('currId', ParseIntPipe) courseId: number) {
+    return this.service.remove(courseId);
+  }
+}
--- src/curriculum/services/curriculum/curriculum.service.ts
+++ src/curriculum/services/curriculum/curriculum.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Curriculum } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class CurriculumService {}
+export class CurriculumService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  //both mentor and apprentice can invoke this
+  async findAll(currID: number) {
+    return this.dataBase.curriculum.findMany({
+      where: {
+        curr_id: currID,
+      },
+    });
+  }
+
+  //this privilege only resides with the mentor ; so setup auth as such
+  async create(curr: Curriculum) {
+    return this.dataBase.curriculum.create({ data: curr });
+  }
+
+  async remove(currId: number) {
+    try {
+      return await this.dataBase.curriculum.delete({
+        where: {
+          curr_id: currId,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to remove currId: ${error.message}`);
+    }
+  }
+
+  async update(currId: number, curr: Curriculum) {
+    return this.dataBase.curriculum.update({
+      where: {
+        curr_id: currId,
+      },
+      data: curr,
+    });
+  }
+}
