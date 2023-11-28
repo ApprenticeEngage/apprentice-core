9ce53ac12d225023a5c14979ab1e181988737aab
EggsyOnCode <farzansaqib2003@gmail.com>
2023-11-28 17:13:47 +0500

    add controller and services for sections

--- src/curriculum/controller/section/section.controller.ts
+++ src/curriculum/controller/section/section.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Section } from '@prisma/client';
+import { SectionService } from 'src/curriculum/services/section/section.service';
 
-@Controller('section')
-export class SectionController {}
+@Controller('curriculum/section')
+export class SectionController {
+  constructor(private readonly service: SectionService) {}
+
+  //find all sections associated with a curriculum
+  @Get('/:currId')
+  async getSectionsPerCurriculum(
+    @Param('currId', ParseIntPipe) currId: number,
+  ) {
+    return this.service.findAll(currId);
+  }
+
+  //find a specific sections associated with a curriculum
+  @Get('/:currId/:secId')
+  async getSection(
+    @Param('currId', ParseIntPipe) currId: number,
+    @Param('secId', ParseIntPipe) secId: number,
+  ) {
+    return this.service.findOne(currId, secId);
+  }
+
+  //store LO of a Section
+  @Post()
+  create(@Body() curr: Section) {
+    return this.service.create(curr);
+  }
+
+  //update a specific learning outcome
+  @Put('/:currId/:secId')
+  update(
+    @Param('secId', ParseIntPipe) secId: number,
+    @Param('currId', ParseIntPipe) currId: number,
+    @Body() Section: Section,
+  ) {
+    return this.service.update(secId, currId, Section);
+  }
+
+  //remove a specific LO
+  @Delete('/:currId/:secId')
+  delete(
+    @Param('secId', ParseIntPipe) secId: number,
+    @Param('currId', ParseIntPipe) currId: number,
+  ) {
+    return this.service.remove(secId, currId);
+  }
+}
--- src/curriculum/services/section/section.service.ts
+++ src/curriculum/services/section/section.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { Section } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class SectionService {}
+export class SectionService {
+  constructor(private readonly dataBase: DatabaseService) {}
+
+  //both mentor and apprentice can invoke this
+
+  //the where clause should intake curr_id_section_id ; theres an err
+  async findAll(currId: number) {
+    return this.dataBase.section.findMany({
+      where: {
+        curr_id: currId,
+      },
+    });
+  }
+
+  async findOne(currId: number, secId: number) {
+    return this.dataBase.section.findMany({
+      where: {
+        curr_id: currId,
+        section_id: secId,
+      },
+    });
+  }
+
+  //this privilege only resides with the mentor ; so setup auth as such
+  async create(curr: Section) {
+    return this.dataBase.section.create({ data: curr });
+  }
+
+  async remove(secId: number, currId: number) {
+    try {
+      return await this.dataBase.section.delete({
+        where: {
+          curr_id_section_id: {
+            curr_id: currId,
+            section_id: secId,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to remove secId: ${error.message}`);
+    }
+  }
+
+  async update(secId: number, currId: number, curr: Section) {
+    return this.dataBase.section.update({
+      where: {
+        curr_id_section_id: {
+          curr_id: currId,
+          section_id: secId,
+        },
+      },
+      data: curr,
+    });
+  }
+}
